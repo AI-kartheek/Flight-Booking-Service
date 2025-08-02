1c2a4b3e557750e670811c4bec912c87bd42e3ec
Kartheek <ai07kartheek@gmail.com>
2025-08-02 19:24:11 +0530

    Added idempotent keys to secure making duplicate payments. Updated 30min cron job code correctly.

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,6 +2,10 @@ const { StatusCodes } = require("http-status-codes");
 
 const { BookingService } = require("../services");
 const { SuccessResponse, ErrorResponse } = require("../utils/common");
+const AppError = require("../utils/errors/app-error");
+
+// this is an in-memory object where it acts like a db to store the idempotent keys to avoid duplicate transactions.
+const inMemDb = {};
 
 async function createBooking(req, res) {
     try {
@@ -25,11 +29,19 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
     try {
+        const idempotencyKey = req.headers['x-idempotency-key'];
+        if (!idempotencyKey) {
+            throw new AppError('Idempotent key is missing', StatusCodes.BAD_REQUEST);
+        }
+        if (inMemDb[idempotencyKey]) {
+            throw new AppError('Cannot retry on a successful payment', StatusCodes.BAD_REQUEST);
+        }
         const response = await BookingService.makePayment({
             totalCost: req.body.totalCost,
             userId: req.body.userId,
             bookingId: req.body.bookingId,
         });
+        inMemDb[idempotencyKey] = idempotencyKey;
         SuccessResponse.data = response;
         return res
             .status(StatusCodes.OK)
